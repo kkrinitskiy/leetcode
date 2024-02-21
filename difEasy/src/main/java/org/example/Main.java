5e513492279a6a646b8a8728becf9ef78f9606b4
Konstantin Krinitskiy <renesansguitar@gmail.com>
2024-02-21 17:00:30 +0300

    14. Longest Common Prefix done

--- difEasy/src/main/java/org/example/Main.java
+++ difEasy/src/main/java/org/example/Main.java
@@ -1,16 +1,20 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.example.IsPalindrome.isPalindrome;
+import static org.example.LongestCommonPrefix.longestCommonPrefix;
 import static org.example.RomanToInt.romanToInt;
 
 public class Main {
     public static void main(String[] args) {
 
-        isPalindrome(1662);
-        romanToInt("MCMXCIV");
-
-
+//        isPalindrome(1662);
+//        romanToInt("MCMXCIV");
+//        longestCommonPrefix(new String[]{"a", "a", "b"});
     }
+
 
 
 
