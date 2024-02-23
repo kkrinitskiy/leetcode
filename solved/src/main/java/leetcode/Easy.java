5954baebd425d5abeef3ef73dbbfe64d520c6553
Konstantin Krinitskiy <kkrinitskiy@internet.ru>
2024-02-23 16:22:34 +0300

    1/10 done

--- solved/src/main/java/leetcode/Easy.java
+++ solved/src/main/java/leetcode/Easy.java
@@ -135,4 +135,30 @@ public class Easy {
         }
         return stack.isEmpty();
     }
+
+    private static class ParkingSystem {
+        int big, medium, small;
+
+        public ParkingSystem(int big, int medium, int small) {
+            this.big = big;
+            this.medium = medium;
+            this.small = small;
+        }
+
+        public boolean addCar(int carType) {
+            if(carType == 1 && big > 0){
+                big -= 1;
+                return true;
+            }
+            if(carType == 2 && medium > 0){
+                medium -= 1;
+                return true;
+            }
+            if(carType == 3 && small > 0){
+                small -= 1;
+                return true;
+            }
+            return false;
+        }
+    }
 }
